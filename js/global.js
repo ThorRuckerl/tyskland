5feb18858f0d17723f26a842161833166f962d55
ThorRuckerl <100678314+ThorRuckerl@users.noreply.github.com>
2022-12-06 13:50:59 +0100

    Setting up the subtheme

---
+++ js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.tyskland = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
